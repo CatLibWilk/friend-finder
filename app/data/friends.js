996fbe806750a1f83057f609dd6b5bdd325d3b43
Andy Wilk <andrewdwilk@gmail.com>
2018-08-08 14:25:00 -0400

    added friends to compare

---
+++ app/data/friends.js
@@ -0,0 +1,88 @@
+var tony = {
+    "name": "Tony",
+    "photo": "",
+    "scores": [
+        3,
+        3,
+        4,
+        3,
+        5,
+        2,
+        4,
+        3,
+        2,
+        5
+    ]
+}
+var tiny = {
+    "name": "Tiny",
+    "photo": "",
+    "scores": [
+        5,
+        5,
+        3,
+        4,
+        2,
+        5,
+        3,
+        3,
+        4,
+        2
+    ]
+}
+var ricky = {
+    "name": "Ricky",
+    "photo": "",
+    "scores": [
+        3,
+        3,
+        2,
+        3,
+        4,
+        2,
+        3,
+        5,
+        3,
+        5
+    ]
+}
+var ticky = {
+    "name": "Ticky",
+    "photo": "",
+    "scores": [
+        2,
+        1,
+        2,
+        3,
+        5,
+        2,
+        4,
+        4,
+        2,
+        5
+    ]
+}
+var fernando = {
+    "name": "Fernando",
+    "photo": "",
+    "scores": [
+        2,
+        3,
+        4,
+        5,
+        5,
+        2,
+        1,
+        1,
+        2,
+        5
+    ]
+}
+
+var compArray = [
+    tony,
+    tiny,
+    ricky,
+    ticky,
+    fernando
+]
